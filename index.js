1650b11dce9bb58f64dba1756ab98481db5b6fa9
Muhiddin Kurbonov <muhiddindc@gmail.com>
2023-04-15 13:47:46 -0500

    add dummy todos, adapt code to use the dummy json

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+import todos from "./todos.js";
 const btnAdd = document.getElementById("btn-add");
 const btnCancel = document.getElementById("btn-cancel");
 const itemForm = document.getElementById("item-form");
@@ -5,9 +6,8 @@ const itemForm = document.getElementById("item-form");
 const itemInput = document.getElementById("item-input");
 const categoryInput = document.getElementById("category-input");
 
-const btnSubmit = document.getElementById("btn-submit");
-
 const incompleteSection = document.getElementById("incomplete-section");
+const completeSection = document.getElementById("complete-section");
 
 const form = document.querySelector(".form-container");
 
@@ -19,12 +19,14 @@ function closeItemForm() {
   itemForm.style.display = "none";
 }
 
-function createTodoHtml(item, cat) {
+function createTodoHtml(item, category, completed) {
   const todo = document.createElement("div");
   todo.setAttribute("class", "todo");
 
   const inputCheckbox = document.createElement("input");
   inputCheckbox.setAttribute("type", "checkbox");
+  completed ? inputCheckbox.setAttribute("checked", true) : "";
+
   todo.appendChild(inputCheckbox);
 
   const description = document.createElement("div");
@@ -34,23 +36,34 @@ function createTodoHtml(item, cat) {
   itemName.innerText = item;
   const itemCategory = document.createElement("p");
   itemCategory.setAttribute("class", "category");
-  itemCategory.innerText = cat;
+  itemCategory.innerText = category;
+
   description.appendChild(itemName);
   description.appendChild(itemCategory);
   todo.appendChild(description);
-  incompleteSection.appendChild(todo);
+
+  completed
+    ? completeSection.appendChild(todo)
+    : incompleteSection.appendChild(todo);
 }
 
 function handleItem(event) {
   event.preventDefault();
   const itemValue = itemInput.value;
   const categoryValue = categoryInput.value;
-  createTodoHtml(itemValue, categoryValue);
+  createTodoHtml(itemValue, categoryValue, false);
   itemInput.value = "";
   categoryInput.value = "";
   closeItemForm();
 }
 
+function showTodos() {
+  todos.forEach((item) => {
+    createTodoHtml(item.todo, item.category, item.completed);
+  });
+  console.log(todos);
+}
+showTodos();
 btnAdd.addEventListener("click", openItemForm);
 btnCancel.addEventListener("click", closeItemForm);
 form.addEventListener("submit", handleItem);
